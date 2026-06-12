04d75a605d991908a5d0ccecb49018e0746fb038
mohamedlarbireda18-cmd <mohamedlarbireda18@gmail.com>
2026-06-12 12:53:13 +0100

    Fix

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Écoute sur toutes les interfaces réseau
+    host: '0.0.0.0',
     port: 3000,
-    strictPort: true,
-    open: false
+    strictPort: true
   }
 })
